b0d1f09f522614cc01b6a357667cdf67b27e6477
Hadassah Uche <uhadassah1@gmail.com>
2023-11-01 15:27:30 +0200

    Cleaned up the code and added useful comments

--- UcheHadassah2_4/HadassahUche2_4.cpp
+++ UcheHadassah2_4/HadassahUche2_4.cpp
@@ -1,22 +1,21 @@
-/*Write the following function to display an integer in reverse order:
-void reverse(int number)
-For example, reverse(3456) displays 6543. Test your function.*/
+/*Name:Uche Hadassah
+This program displays a user-given integer in reverse order*/
 #include<iostream>
 using namespace std;
-void reverse(int number);
+void reverse(int number);//Function Prototype
 int main()
 {
 	int num;
 	cout << "Enter the integer you want to reverse:";
 	cin >> num;
-	reverse(num);
+	reverse(num);//Calling the function
 	return 0;
 }
 void reverse(int number)
 {
-	while (number % 10 != 0)
+	while (number % 10 != 0)//While we have not reached the first number
 	{
-		cout << number % 10;
-		number = number / 10;
+		cout << number % 10;//Outputs the last number
+		number = number / 10;//Removes the last number
 	}
 }
